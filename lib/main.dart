1f2e93b42b2bea095cc8b1023de3da6e275ab123
Enver Tuna Sönmez <etsonmez01@gmail.com>
2024-12-18 23:43:31 +0300

    add - fireabse initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_authentication/firebase_options.dart';
 import 'package:firebase_authentication/products/navigation/app_router.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +7,7 @@ import 'package:firebase_authentication/providers.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(
     MultiBlocProvider(
       providers: Providers.providers,
